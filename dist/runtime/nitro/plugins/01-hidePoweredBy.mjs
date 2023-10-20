fa049df4ae136969e3c9a8255b200f50335a301b
Sébastien Raffray <sraffray@yahoo.fr>
2023-10-20 16:58:44 +0200

    remove dist

--- dist/runtime/nitro/plugins/01-hidePoweredBy.mjs
+++
@@ -1,7 +0,0 @@
-export default (function(nitro) {
-  nitro.hooks.hook("render:response", (response) => {
-    if (response.headers["x-powered-by"]) {
-      delete response.headers["x-powered-by"];
-    }
-  });
-});
